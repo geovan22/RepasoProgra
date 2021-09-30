71e6096a945620db0411c6ea4df5445ca1c2bfd3
geovan22 <43213313+geovan22@users.noreply.github.com>
2021-09-30 07:51:42 -0600

    Add files via upload

---
+++ Mayores.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Mayores
+    {
+        private int n1 = 0;
+        private int n2 = 0;
+        private int MayorA(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        public int MayorC()
+        {
+            if (n1 > n2)
+            {
+                return n1;
+            }
+            else
+            {
+                return n2;
+            }
+        }
+
+        public int MayorC(int Val)
+        {
+            return Val;
+        }
+    }
+}
